e0c9305ec1ec9026e75017820bad9ee09c4fbe78
praneethgs36 <praneethgs36@gmail.com>
2025-05-04 15:24:52 +0530

    changes

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,29 +1,32 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.24;
+pragma solidity 0.8.18;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-    
-    uint256 public myFavoriteNumber;
-    
+    uint256 myFavoriteNumber;
+
     struct Person {
-        uint256 favoriteNumbers;
+        uint256 favoriteNumber;
         string name;
     }
-
-    // Person public pat = Person(7, "Pat");
+    // uint256[] public anArray;
     Person[] public listOfPeople;
-    
+
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
-    
+
     function retrieve() public view returns (uint256) {
         return myFavoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
-    } 
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
